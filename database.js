72a914f5f78cd059eda4879d20d6d07b3fc4c155
Katrina Nguyen <katrinan@usc.edu>
2021-06-29 17:39:41 -0700

    updating pw file

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
-// const postgresPW = require("./config.js");
+const postgresPW = require("./config.js");
 
 const Sequelize = require("sequelize");
 
 const user = "postgres";
-const password = "postgres";
+const password = postgresPW.password;
 const host = "localhost";
 const database = "testingpassport";
 const sequelize = new Sequelize(database, user, password, {
